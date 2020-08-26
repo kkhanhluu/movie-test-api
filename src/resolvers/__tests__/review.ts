495c03e2f05198ff8e39268773331bbc377040e7
Khanh Luu <luukhanh96@gmail.com>
2020-08-26 13:58:36 +0200

    add test for addReview

---
+++ src/resolvers/__tests__/review.ts
@@ -0,0 +1,66 @@
+import * as setup from "../../__tests__/setup";
+import { ReviewModel, MovieModel } from "../../models";
+import { addReview } from "../review";
+let testMongo: setup.TestMongoConn;
+let testMovie;
+beforeEach(async () => {
+  testMongo = await setup.beforeEach();
+
+  testMovie = new MovieModel({
+    name: "Test Movie",
+    duration: 90,
+    releaseDate: "1598438370837",
+    actors: [],
+    coverImage:
+      "https://image.tmdb.org/t/p/w500/e7ZsW5EbLbQwoGx0548KCmCAXA9.jpg",
+    author: "5f4594c7fd480639e8d889e9",
+  });
+
+  await testMovie.save();
+});
+
+afterEach(() => setup.afterEach(testMongo));
+
+describe("Test create review", () => {
+  it("should create a review", async () => {
+    const reviewDoc = await addReview(
+      undefined,
+      {
+        rating: 3,
+        review: "Very good",
+        user: "5f4594c7fd480639e8d889e9",
+        movie: testMovie.id,
+      },
+      {
+        userInfo: {
+          id: "5f4594c7fd480639e8d889e9",
+          username: "Test User",
+        },
+      },
+    );
+
+    expect(reviewDoc).toBeDefined();
+    expect(reviewDoc.review).toEqual("Very good");
+    expect(reviewDoc.rating).toEqual(3);
+  });
+
+  it("should not create a review", async () => {
+    let error;
+    try {
+      await addReview(
+        undefined,
+        {
+          rating: 3,
+          review: "Very good",
+          user: "5f4594c7fd480639e8d889e9",
+          movie: testMovie.id,
+        },
+        null,
+      );
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toEqual(new Error("Not authenticated"));
+  });
+});
---
+++ src/resolvers/review.ts
@@ -0,0 +1,7 @@
+import { Context, ReviewResponse } from "src/types";
+
+export async function addReview(
+  _: void,
+  args: any,
+  ctx: Context,
+): Promise<ReviewResponse> {}
