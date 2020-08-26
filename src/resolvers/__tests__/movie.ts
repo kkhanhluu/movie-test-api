45804ab541178798f8f6215cf6ecf9b24aab9967
Khanh Luu <luukhanh96@gmail.com>
2020-08-26 12:59:13 +0200

    add test for get one movie

--- src/resolvers/__tests__/movie.ts
+++ src/resolvers/__tests__/movie.ts
@@ -1,6 +1,6 @@
 import * as setup from "../../__tests__/setup";
 import { MovieModel } from "../../models";
-import { movies } from "../movie";
+import { movies, movie } from "../movie";
 let testMongo: setup.TestMongoConn;
 
 beforeEach(async () => {
@@ -38,5 +38,38 @@ describe("Test get movies", () => {
     expect(response.length).toEqual(2);
     expect(response[0].name).toEqual("Test Movie 1");
     expect(response[1].name).toEqual("Test Movie 2");
+  });
+});
+
+describe("Test get one movie", () => {
+  it("should return one movie and movie should populate reviews", async () => {
+    const testMovie = new MovieModel({
+      name: "Test Movie ",
+      duration: 90,
+      releaseDate: "1598438370837",
+      actors: [],
+      coverImage:
+        "https://image.tmdb.org/t/p/w500/e7ZsW5EbLbQwoGx0548KCmCAXA9.jpg",
+      author: "5f4594c7fd480639e8d889e9",
+    });
+
+    await testMovie.save();
+
+    const response = movie(undefined, { id: testMovie.id });
+    expect(response).toBeDefined();
+    expect(response.name).toEqual("Test Movie");
+    expect(response.duration).toEqual(90);
+    expect(response.reviews).toBeDefined();
+  });
+
+  it("should throw error if the movie does not exist", async () => {
+    let error, response;
+    try {
+      response = movie(undefined, { id: "5f462df07ecef6b11c50fe5b" });
+    } catch (e) {
+      error = e;
+    }
+    expect(response).toBeUndefined();
+    expect(error).toEqual(new Error("Cannot find the movie"));
   });
 });
--- src/resolvers/movie.ts
+++ src/resolvers/movie.ts
@@ -4,3 +4,5 @@ import { MovieResponse } from "src/types";
 export async function movies(_: void): Promise<MovieResponse[]> {
   return await MovieModel.find();
 }
+
+async function movie(_: void, args: any): Promise<MovieResponse> {}
